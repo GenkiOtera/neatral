6abed20a491afb7d93888bcd859b4e071a447e3f
KINGE <g.otera.development@gmail.com>
2022-07-17 01:11:02 +0900

    アプリ構成を複数モジュールに変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// #region NgRx
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-// #endregion
+import { MainModule } from './modules/main/main.module';
 
 @NgModule({
   declarations: [
@@ -16,10 +15,9 @@ import { EffectsModule } from '@ngrx/effects';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // #region NgRx
     StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([])
-    // #endregion
+    EffectsModule.forRoot([]),
+    MainModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+})
+export class MainComponent implements OnInit {
+
+  title = 'neatral';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
