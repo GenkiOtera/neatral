ae8ca6549aadee6d4194e398bd77919ed39da7d8
KINGE <g.otera.development@gmail.com>
2022-07-17 00:35:47 +0900

    NgRx環境の構築

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// #region NgRx
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+// #endregion
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // #region NgRx
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([])
+    // #endregion
   ],
   providers: [],
   bootstrap: [AppComponent]
